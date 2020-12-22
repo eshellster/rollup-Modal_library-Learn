3f6d9d3f5d87146c3d5e65b3bf25055fb2fe5bdb
eshell <eshellster@gmail.com>
2020-12-22 17:05:30 +0900

    바벨

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+    plugins: ['@typescript-eslint', 'prettier', 'jsdoc'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:jsdoc/recommended',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
+    rules: {},
+    overrides: [
+        {
+            files: ['src/*/*'],
+            rules: {
+                'max-lines': 'off',
+                'max-nested-callbacks': 'off',
+                'max-statements': 'off',
+            },
+        },
     ],
-    "rules": {
-    }
+    settings: {
+        node: {
+            extensions: ['.ts', '.json'],
+        },
+    },
 };
