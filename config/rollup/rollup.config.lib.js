4b7ae0de79be683c75d426484a79589221eb4abe
eshell <eshellster@gmail.com>
2021-01-06 23:34:53 +0900

    fix: 초간단

--- config/rollup/rollup.config.lib.js
+++ config/rollup/rollup.config.lib.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 
-import pkg from './package.json';
+import pkg from '../../package.json';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
